3ab94d6b9a5ae9be938fd1be3157d2ba25bc90cf
willsf2021 <willsf2021@gmail.com>
2024-08-28 00:09:38 -0300

    Validation on JavaScript Vanilla added

--- script.js
+++ script.js
@@ -7,47 +7,93 @@ let radioError = document.querySelector(".errorMessageQuery");
 
 let checkBox = document.querySelector("#checkbox");
 let checkBoxError = document.querySelector(".errorMessageConsent");
-let hasError = false;
+
+let dialog = document.querySelector("#modal");
+// submitButton.addEventListener("click", () => {
+//   for (let i = 0; i < arrayError.length; i++) {
+//     if (arrayInputsText[i].value == "") {
+//       arrayError[i].style.display = "block";
+//       arrayInputsText[i].style.borderColor = "red";
+//     }
+//   }
+
+//   function returnTrueOrFalse() {
+//     for (let i = 0; i < radioNameInput.length; i++) {
+//       if (radioNameInput[i].checked) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+
+//   if (!returnTrueOrFalse()) {
+//     radioError.style.display = "block";
+//   }
+//   if (!checkBox.checked) {
+//     checkBoxError.style.display = "block";
+//   }
+// });
 
 submitButton.addEventListener("click", (event) => {
-  for (let i = 0; i < arrayError.length; i++) {
-    if (arrayInputsText[i].value == "") {
-      arrayError[i].style.display = "block";
-      arrayInputsText[i].style.borderColor = "red";
-    }
+  event.preventDefault();
+
+  let formIsValid = true;
+
+  function isEmailValid(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
   }
 
-  function returnTrueOrFalse() {
-    for (let i = 0; i < radioNameInput.length; i++) {
-      if (radioNameInput[i].checked) {
-        return true;
+  for (let i = 0; i < arrayInputsText.length; i++) {
+    if (arrayInputsText[i].type === "email") {
+      if (!isEmailValid(arrayInputsText[i].value)) {
+        arrayError[i].style.display = "block";
+        arrayInputsText[i].style.borderColor = "red";
+        formIsValid = false;
+      } else {
+        arrayError[i].style.display = "none";
+        arrayInputsText[i].style.borderColor = "";
+      }
+    } else {
+      if (arrayInputsText[i].value === "") {
+        arrayError[i].style.display = "block";
+        arrayInputsText[i].style.borderColor = "red";
+        formIsValid = false;
+      } else {
+        arrayError[i].style.display = "none";
+        arrayInputsText[i].style.borderColor = "";
       }
     }
-    return false;
   }
 
   if (!returnTrueOrFalse()) {
     radioError.style.display = "block";
+    formIsValid = false;
+  } else {
+    radioError.style.display = "none";
   }
+
   if (!checkBox.checked) {
     checkBoxError.style.display = "block";
+    formIsValid = false;
+  } else {
+    checkBoxError.style.display = "none";
   }
 
-  // if (!hasError) {
-  //   const successDialog = document.getElementById("successDialog");
-  //   successDialog.showModal();
-  // }
-});
+  if (formIsValid) {
+    dialog.showModal();
 
-document.addEventListener("click", (event) => {
-  const clickedElement = event.target;
-
-  if (clickedElement.type == "radio") {
-    return;
+    setTimeout(() => {
+      document.querySelector("form").submit();
+    }, 3000);
   }
 });
 
-// for (let i = 0; i < radioNameInput.length; i++)
-//   if (radioNameInput[i].checked) {
-//     radioNameInput[i].checked = false;
-//   }
+function returnTrueOrFalse() {
+  for (let i = 0; i < radioNameInput.length; i++) {
+    if (radioNameInput[i].checked) {
+      return true;
+    }
+  }
+  return false;
+}
